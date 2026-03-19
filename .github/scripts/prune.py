0eadb8747c74494e0a5ca9b56138648d9c1be006
functionzz <164675620+functionzz@users.noreply.github.com>
2026-03-19 13:46:53 -0400

    Remove string deprecation process

---
+++ .github/scripts/prune.py
@@ -0,0 +1,131 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""
+Prune localization files after updates from Firefox branches.
+
+Expects to find `_data/[project]/[branch]/.json` for each project,
+and removes any other JSON data files in `_data/`.
+Removes any files and messages not used by any branch.
+
+Writes a commit message summary as `.prune_msg`.
+"""
+
+import json
+from argparse import ArgumentParser
+from os import getcwd, remove, scandir
+from os.path import join, relpath, splitext, isdir
+from sys import exit
+from moz.l10n.paths import L10nDiscoverPaths
+from moz.l10n.resource import parse_resource, serialize_resource
+from moz.l10n.model import Entry
+
+
+def prune_file(path: str, msg_refs: set[str]):
+    with open(path, "+rb") as file:
+        resource = parse_resource(path, file.read())
+        drop_count = 0
+        for section in resource.sections:
+            next = [
+                entry
+                for entry in section.entries
+                if not isinstance(entry, Entry)
+                or ".".join(section.id + entry.id) in msg_refs
+            ]
+            diff = len(section.entries) - len(next)
+            if diff > 0:
+                drop_count += diff
+                section.entries = next
+        resource.sections = [
+            section for section in resource.sections if section.entries
+        ]
+        if drop_count:
+            print(f"drop {drop_count} from {path}")
+            file.seek(0)
+            for line in serialize_resource(resource):
+                file.write(line.encode("utf-8"))
+            file.truncate()
+    return drop_count
+
+
+def prune(project: str, branches: list[str]):
+    cwd = getcwd()
+
+    project_path = join(cwd, f"mozilla-mobile-test/{project}")
+    _data_path = f"_data/{project}"
+
+    removed_data = []
+    removed_files = 0
+    removed_messages = 0
+    refs: dict[str, set[str]] = {}
+    expected = set(branches)
+
+    if not isdir(_data_path):
+        exit(f"_data directory does not exist: {_data_path}")
+
+    for entry in scandir(_data_path):
+        branch, ext = splitext(entry.name)
+
+        if entry.is_file() and ext == ".json":
+            if branch in branches:
+
+                expected.remove(branch)
+                with open(entry.path, "r") as file:
+                    data: dict[str, list[str]] = json.load(file)
+
+                for path, keys in data.items():
+                    if path in refs:
+                        refs[path].update(keys)
+                    else:
+                        refs[path] = set(keys)
+            else:
+                print(f"remove {relpath(entry.path, project_path)}")
+                remove(entry.path)
+                removed_data.append(branch)
+    if not refs:
+        exit(f"No data found for: {branches}")
+    if expected:
+        exit(f"Incomplete data! Not found: {expected}")
+
+    for ref_path in L10nDiscoverPaths(cwd, ref_root=project_path).ref_paths:
+        path = relpath(ref_path, cwd)
+        if path not in refs:
+            print(f"remove {path}")
+            remove(path)
+            removed_files += 1
+        elif refs[path]:
+            removed_messages += prune_file(path, refs[path])
+    return removed_data, removed_files, removed_messages
+
+
+def write_commit_msg(data: list[str], files: int, messages: int):
+    summary = []
+    for branch in data:
+        summary.append(f"{branch} data")
+    if files:
+        summary.append(f"{files} file" if files == 1 else f"{files} files")
+    if messages:
+        summary.append(
+            f"{messages} message" if messages == 1 else f"{messages} messages"
+        )
+    with open(".prune_msg", "w") as file:
+        file.write(f"Removed: {', '.join(summary)}" if summary else "no changes")
+
+
+if __name__ == "__main__":
+    prog = "python .github/scripts/prune.py"
+    parser = ArgumentParser(prog=prog, description=__doc__)
+    parser.add_argument(
+        "--project",
+        required=True,
+        help='The project identifier, e.g. "fenix", "android-components", or "focus-android".',
+    )
+    args = parser.parse_args()
+
+    config_file = join(".github", "update-config.json")
+    with open(config_file) as f:
+        cfg_automation = json.load(f)
+
+    removed = prune(args.project, cfg_automation["branches"])
+    write_commit_msg(*removed)
---
+++ .github/scripts/update.py
@@ -0,0 +1,192 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""
+Update the localization source files from an Android-related branch, adding new files and
+messages. For updates from the "{HEAD}" branch, also update changed messages.
+
+Writes a summary of the branch's localized files and message keys as
+`_data/[project]/[branch].json`, and a commit message summary as `.update_msg`.
+"""
+
+import json
+from argparse import ArgumentParser
+from filecmp import cmp
+from os import makedirs
+from os.path import abspath, dirname, exists, join, relpath
+from shutil import copy
+from sys import exit
+from typing import TypedDict
+
+from moz.l10n.formats import UnsupportedFormat
+from moz.l10n.paths import L10nConfigPaths
+from moz.l10n.resource import (
+    add_entries,
+    parse_resource,
+    serialize_resource,
+)
+from moz.l10n.model import Entry
+
+
+class AutomationConfig(TypedDict):
+    branches: list[str]
+    head: str
+    paths: list[str]
+
+def update(
+    cfg_automation: AutomationConfig,
+    project: str,
+    branch: str,
+    fx_root: str,
+    config_files: list[str],
+):
+    if project not in ("android-components", "fenix", "focus-android"):
+        exit(f"Unknown project: {project}")
+    if branch not in cfg_automation["branches"]:
+        exit(f"Unknown branch: {branch}")
+    is_head = branch == cfg_automation["head"]
+    if not exists(fx_root):
+        exit(f"Firefox root not found: {fx_root}")
+    print(f"source: {branch} at {fx_root}")
+    fx_root = abspath(fx_root)
+
+    source_dirs: set[str] = set()
+    source_files: set[str] = set()
+
+
+    for cfg_name in config_files:
+        cfg_path = join(fx_root, cfg_name)
+
+        if not exists(cfg_path):
+            exit(f"Config file not found: {cfg_path}")
+        paths = L10nConfigPaths(cfg_path)
+        source_files.update(fx_path for fx_path, _ in paths.all())
+
+
+    messages: dict[str, list[str]] = {}
+    new_files = 0
+    updated_files = 0
+    for fx_path in source_files:
+        rel_path = "mozilla-mobile" + relpath(fx_path, fx_root).replace("mobile/android", "")
+        makedirs(dirname(rel_path), exist_ok=True)
+
+        try:
+            fx_res = parse_resource(fx_path)
+        except UnsupportedFormat:
+            messages[rel_path] = []
+            if not exists(rel_path):
+                print(f"create {rel_path}")
+                copy(fx_path, rel_path)
+                new_files += 1
+            elif branch == cfg_automation["head"] and not cmp(fx_path, rel_path):
+                print(f"update {rel_path}")
+                copy(fx_path, rel_path)
+                updated_files += 1
+            else:
+                # print(f"skip {rel_path}")
+                pass
+            continue
+
+        messages[rel_path] = [
+            ".".join(section.id + entry.id)
+            for section in fx_res.sections
+            for entry in section.entries
+            if isinstance(entry, Entry)
+        ]
+
+        if not exists(rel_path):
+            print(f"create {rel_path}")
+            with open(rel_path, "+wb") as file:
+                for line in serialize_resource(fx_res):
+                    file.write(line.encode("utf-8"))
+            new_files += 1
+        elif cmp(fx_path, rel_path):
+            # print(f"equal {rel_path}")
+            pass
+        else:
+            with open(rel_path, "+rb") as file:
+                res = parse_resource(rel_path, file.read())
+                if add_entries(res, fx_res, use_source_entries=is_head):
+                    print(f"update {rel_path}")
+                    file.seek(0)
+                    for line in serialize_resource(res):
+                        file.write(line.encode("utf-8"))
+                    file.truncate()
+                    updated_files += 1
+                else:
+                    # print(f"unchanged {rel_path}")
+                    pass
+
+    data_path = join(f"_data/{project}", f"{branch}.json")
+    makedirs(dirname(data_path), exist_ok=True)
+    with open(data_path, "w") as file:
+        json.dump(messages, file, indent=2, sort_keys=True)
+
+    return new_files, updated_files, sorted(list(source_dirs))
+
+
+def write_commit_msg(args, new_files: int, updated_files: int):
+    new_str = f"{new_files} new" if new_files else ""
+    update_str = f"{updated_files} updated" if updated_files else ""
+    summary = (
+        f"{new_str} and {update_str}"
+        if new_str and update_str
+        else new_str or update_str or "no changes"
+    )
+    count = updated_files or new_files
+    summary += " files" if count > 1 else " file" if count == 1 else ""
+    head = f"{args.branch} ({args.commit})" if args.commit else args.branch
+    with open(".update_msg", "w") as file:
+        file.write(f"{head}: {summary}")
+
+
+if __name__ == "__main__":
+    config_file = join(".github", "update-config.json")
+    with open(config_file) as f:
+        cfg_automation = json.load(f)
+
+    prog = "python .github/scripts/update.py"
+    parser = ArgumentParser(
+        prog=prog,
+        description=__doc__.format(HEAD=cfg_automation["head"]),
+        epilog=f"""Example: {prog} --branch release --firefox ../firefox
+        --configs browser/locales/l10n.toml mobile/android/locales/l10n.toml""",
+    )
+    parser.add_argument(
+        "--project",
+        required=True,
+        help='The project identifier, e.g. "fenix", "android-components", or "focus-android".',
+    )
+    parser.add_argument(
+        "--branch",
+        required=True,
+        help='The branch identifier, e.g. "main", "beta", or "release".',
+    )
+    parser.add_argument("--commit", help="A commit id for the branch.")
+    parser.add_argument(
+        "--firefox", required=True, help="Path to the root of the Firefox source tree."
+    )
+    parser.add_argument(
+        "--configs",
+        metavar="C",
+        nargs="+",
+        required=True,
+        help="Relative paths from the Firefox root to the l10n.toml files.",
+    )
+    args = parser.parse_args()
+
+    new_files, updated_files, source_dirs = update(
+        cfg_automation, args.project, args.branch, args.firefox, args.configs
+    )
+
+    if cfg_automation["paths"] != source_dirs:
+        # Write back updated configuration, making sure that the list of paths
+        # is a superset of the configuration across all branches.
+        cfg_automation["paths"] = sorted(
+            list(set(cfg_automation["paths"] + source_dirs))
+        )
+        with open(config_file, "w") as file:
+            json.dump(cfg_automation, file, indent=2, sort_keys=True)
+
+    write_commit_msg(args, new_files, updated_files)
